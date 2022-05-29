b581c94bba2bf3aa693dc8da990d6d691668f981
Tsai-Chieh <d0177381@gmail.com>
2022-05-29 16:48:04 +0800

    :sparkles: add js effect in question-btn

--- js/index.js
+++ js/index.js
@@ -2,3 +2,44 @@
 $(".top").on("click", function(event) {
     $("html, body").animate({ scrollTop: 0 }, 500);
 });
+
+const project = [{
+            "userNum": "10000",
+            "basePrice": 600,
+            "proPrice": 1600,
+        },
+        {
+            "userNum": "15000",
+            "basePrice": 1200,
+            "proPrice": 2400,
+        },
+        {
+            "userNum": "20000",
+            "basePrice": 1800,
+            "proPrice": 3200,
+        },
+        {
+            "userNum": "25000",
+            "basePrice": 2400,
+            "proPrice": 4000,
+        },
+        {
+            "userNum": ">25000",
+            "basePrice": 3000,
+            "proPrice": 4800,
+        }
+    ]
+    // price btn
+$('.btn-price').click(function() {
+    $('.btn-price').removeClass('active');
+    const idx = $(this).index()
+    $(this).addClass('active');
+    $('span.user-count').text(project[idx].userNum)
+    $('.base > span.price-count').text(project[idx].basePrice)
+    $('.standard > span.price-count').text(project[idx].proPrice)
+});
+
+// toggle question
+$('.toggle-btn').click(function() {
+    $(this).parent().toggleClass('active');
+});
